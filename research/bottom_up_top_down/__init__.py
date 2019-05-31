99ff8f6224cf66954c573c07506c3b9b4ddca3e9
yugeTen <jimmyshi820@gmail.com>
2019-05-31 16:07:07 +0100

    update

--- research/bottom_up_top_down/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint
-
-_bottom_up_top_down = Blueprint('bottom_up_top_down', __name__, template_folder='templates', static_folder='static', static_url_path='/static')
-
--- research/rfs_vs_nns/__init__.py
+++
@@ -1,4 +0,0 @@
-from flask import Blueprint 
-
-_rfs_vs_nns = Blueprint('rfs_vs_nns', __name__, template_folder='templates', static_folder='static', static_url_path='/static')
-
